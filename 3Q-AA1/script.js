0589c7134affe3d197f836df942d79abbc102d29
blzy12 <bmdasuncion22@src.pshs.edu.ph>
2025-01-24 08:06:24 +0800

    Update script.js

--- 3Q-AA1/script.js
+++ 3Q-AA1/script.js
@@ -1,16 +1,14 @@
 const Pi = 3.14; 
 let rad = 6378e3;
+let circ = (2 * Pi * rad);
+let surfarea = (4 * Pi * (rad ** 2));
+let volume = ((4/3)*Pi*(rad ** 3));
+
 document.write('The radius of the sphere is ' + rad);
 document.write('<br>');
-
-let circ = (2 * Pi * rad);
 document.write('The circumference is ' + circ);
 document.write('<br>');
-
-let surfarea = (4 * Pi * rad ** 2);
 document.write('The surface area is ' + surfarea);
 document.write('<br>');
-
-let volume = ((4/3)*Pi*(rad ** 3));
 document.write('The volume is ' + volume);
 document.write('<br>');
